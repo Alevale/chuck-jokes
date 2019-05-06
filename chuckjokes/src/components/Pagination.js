002f15ae5fe39b4e87f4e66fed6f9f7b7aaad528
alevale <alexvales111@gmail.com>
2019-05-06 15:59:49 +0200

    feat(Pagination): Add the pagination component

---
+++ chuckjokes/src/components/Pagination.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
+import IconButton from '@material-ui/core/IconButton'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+
+const Pagination = ({ page, favoriteJokes, pageUp, pageDown }) => (
+    <Grid item xs={ 12 }>
+        <Grid container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+              wrap="nowrap"
+              spacing={ 8 }>
+            <Grid item sm={ 1 }>
+                <IconButton aria-label="Go Back" onClick={ pageDown } disabled={ page === 0 } color="secondary">
+                    <ChevronLeft/>
+                </IconButton>
+            </Grid>
+            <Grid item sm={ 1 }>
+                { page + 1 }
+            </Grid>
+            <Grid item sm={ 1 }>
+                <IconButton aria-label="Go Forward" onClick={ pageUp }
+                            disabled={ favoriteJokes.length / 10 < (page + 1) } color="secondary">
+                    <ChevronRight/>
+                </IconButton>
+            </Grid>
+        </Grid>
+    </Grid>
+)
+
+Pagination.propTypes = {
+    favoriteJokes: PropTypes.array.isRequired,
+    page: PropTypes.number.isRequired,
+    pageUp: PropTypes.func.isRequired,
+    pageDown: PropTypes.func.isRequired
+}
+
+export default Pagination
