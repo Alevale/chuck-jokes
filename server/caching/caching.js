90f52a588a61dcbe703d195d43dc7b214bcc8e83
alevale <alexvales111@gmail.com>
2019-05-06 15:54:55 +0200

    feat(): Add cache functionalities

--- server/caching/caching.js
+++ server/caching/caching.js
@@ -6,6 +6,9 @@ const caching = require('express').Router();
 const logger = require('../utils/logger');
 const cacheController = require('./controller/caching-controller');
 
+const passport = require('passport');
+const passportController = require('../middleware/passport/controller/passport-controller');
+
 /**
  * @description Clear the Cache
  * @TODO We might want to secure this API ;)
@@ -25,11 +28,76 @@ caching.get('/clear', (req, res) => {
 	}, err => {
 		// Log the Error
 		logger.error('Cache clear failed: ' + JSON.stringify( err) );
-		// Return the error response 
+		// Return the error response
 		res.json({
 			err: 'Cache could not be cleared'
 		});
 	});
+});
+
+/**
+ * @description Get the items in the cache
+ * @method GET
+ * @example http://localhost:3000/cache/get
+ */
+caching.get('/get', (req, res) => {
+
+    passportController.getLoggedInUserObject(req.headers.authorization).then(userObject => {
+        const { email } = userObject;
+        return cacheController
+            .getMemoryCache(email)
+            .then((result) => {
+                // Return the success response
+                return res.json({
+                    result
+                });
+            })
+			.catch(err => {
+                // Log the Error
+                logger.error('Cache error: ' + JSON.stringify(err));
+                // Return the error response
+                res.json({
+                    err: 'Error getting Cache'
+                });
+            });
+    }, () => {
+        return res.status( 403 ).json({
+            err: 'Unauthorized'
+        });
+    });
+});
+
+/**
+ * @description Set the items in the cache
+ * @method POST
+ * @example http://localhost:3000/cache/set
+ */
+caching.post('/set', (req, res) => {
+
+    passportController.getLoggedInUserObject(req.headers.authorization).then(userObject => {
+        const { email } = userObject
+        return cacheController
+            .setMemoryCache(email, req.body)
+            .then((result) => {
+                logger.info('Object saved in memory')
+                // Return the success response
+                return res.json({
+                    result
+                });
+            })
+			.catch(err => {
+                // Log the Error
+                logger.error('Cache error: ' + JSON.stringify(err));
+                // Return the error response
+                return res.status( 400 ).json({
+                    err: 'Item could not be saved in Cache'
+                });
+            });
+    }, () => {
+        return res.status( 403 ).json({
+            err: 'Unauthorized'
+        });
+    });
 });
 
 // Export the module
--- server/caching/controller/caching-controller.js
+++ server/caching/controller/caching-controller.js
@@ -1,22 +1,40 @@
-'use strict';
+'use strict'
 /**
  * Require our modules
  */
-const config = require('../../../config/config');
-const cacheManager = require('cache-manager');
-const memoryCache = cacheManager.caching({ 
-    store: config.caching.store, 
-    max: config.caching.max, 
+const config = require('../../../config/config')
+const cacheManager = require('cache-manager')
+const memoryCache = cacheManager.caching({
+    store: config.caching.store,
+    max: config.caching.max,
     ttl: config.caching.ttl
-});
+})
 
 module.exports = {
     // Reset the Cache
-	clearCache: () => {
-		return memoryCache.reset();
+    clearCache: () => {
+        return memoryCache.reset()
     },
     // Get the Cache
-	getMemoryCache: () => {
-		return memoryCache;
-	}
-};
+    getMemoryCache: (key) => {
+        return new Promise((resolve, reject) => {
+            return memoryCache.get(key, (err, res) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(res);
+            })
+        })
+    },
+    // Set the Cache
+    setMemoryCache: (key, values) => {
+        return new Promise((resolve, reject) => {
+            return memoryCache.set(key, values, (err, res) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(res);
+            })
+        })
+    }
+}
