a71d5293ce119d7b4d50e4539a48c4eeb68d8480
alevale <alexvales111@gmail.com>
2019-05-16 14:11:18 +0200

    impr(chuckJokes): Add he library for decoding htmlentities and remove dangerouslySetInnerHTML

--- chuckjokes/src/components/ChuckJoke.js
+++ chuckjokes/src/components/ChuckJoke.js
@@ -1,32 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import he from 'he'
 import Grid from '@material-ui/core/Grid'
 import IconButton from '@material-ui/core/IconButton'
 import Favorite from '@material-ui/icons/Favorite'
 import FavoriteOutlined from '@material-ui/icons/FavoriteBorder'
 
-const ChuckJoke = ({ favoriteClicked, joke, isFavorite }) => (
+import {removeJoke, addJoke} from '../store/reducers/jokes'
+
+const ChuckJoke = ({ removeJoke, joke, isFavorite, addJoke }) => (
     <Grid container
           direction="row"
           justify="space-between"
           alignItems="center"
           wrap="nowrap"
           spacing={ 8 }
-          style={{ margin: 'auto' }}>
-        <Grid item sm={ 1 } xs={ 2 } onClick={ favoriteClicked }>
-            <IconButton aria-label="Add to favorites" color="secondary">
+          style={ { margin: 'auto' } }>
+        <Grid item sm={ 1 } xs={ 2 } onClick={ () => {
+            isFavorite ? removeJoke(joke) : addJoke(joke)
+        } }>
+            <IconButton aria-label="Add to favorites" color="secondary"
+                        data-test={ isFavorite ? 'removejoke' : 'addjoke' }>
                 { isFavorite ? <Favorite/> : <FavoriteOutlined/> }
             </IconButton>
         </Grid>
-        <Grid item sm={ 11 } xs={ 10 } dangerouslySetInnerHTML={{__html: joke}}>
+        <Grid item sm={ 11 } xs={ 10 }>
+            { he.decode(joke.joke) }
         </Grid>
     </Grid>
 )
 
 ChuckJoke.propTypes = {
-    favoriteClicked: PropTypes.func.isRequired,
-    joke: PropTypes.string.isRequired,
+    joke: PropTypes.object.isRequired,
     isFavorite: PropTypes.bool.isRequired
 }
 
-export default ChuckJoke
+export default connect(
+    (state) => ({ jokes: state.joke.jokes }),
+    { removeJoke, addJoke }
+)(ChuckJoke)
+
